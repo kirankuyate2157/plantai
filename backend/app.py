8cb417a58f89c090e65065cdc331c2378cca6c3c
Kiran Kuyate <kiran.r.k@yugaantar-innovations.com>
2025-03-03 22:37:16 +0530

    DEV: Predict issue fix-3

--- backend/app.py
+++ backend/app.py
@@ -43,7 +43,7 @@ def predict():
 
 if __name__ == "__main__":
     port = int(
-        os.environ.get("PORT", 5000)
+        os.environ.get("PORT", 10000)
     )  # Get Render's assigned port or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
 
